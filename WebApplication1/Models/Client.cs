5ef8fa02bf95728d0bfbdfe2bf105c06858dde16
honmameiko22 <honmameiko22@hotmail.com>
2018-04-28 08:52:01 +0800

    添加GetClients函数

---
+++ WebApplication1/Models/Client.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Client
+    {
+        public int ClientId { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ WebApplication1/Models/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class DataContext: DbContext
+    {
+        public DbSet<Client> Clients { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+    }
+}
